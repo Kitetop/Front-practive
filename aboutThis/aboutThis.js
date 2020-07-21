6cac69312e48ca9e08140d3b467491422fccab0f
Kitetop <33886685+Kitetop@users.noreply.github.com>
2020-07-21 15:26:08 +0800

    (add): JS 关于this指向的问题

---
+++ aboutThis/aboutThis.js
@@ -0,0 +1,24 @@
+var number = 5;
+var obj = {
+    number: 3,
+    fn1: (function () {
+        var number;
+        this.number *= 2; // 5 * 2 = 10;
+        number = number * 2;
+        console.log(this.number, 'test====');
+        number = 3;
+        return function () {
+            var num = this.number;
+            this.number *= 2;
+            console.log(num, 'this.number');
+            number *= 3;
+            console.log(number, 'father number');
+        }
+    })() // 在调用时相当于普通函数执行，因此其this指向全局的window
+}
+var fn1 = obj.fn1; // 等于普通函数执行，this指向window， 此时window.number = 10;
+fn1.call(null); // 默认绑定规则 => num = 全局下的number = 10, number的值则要去上一层作用域去寻找， 3 * 3 = 9；此时window.number = 20
+let test = new fn1(); // this是函数自身的this，并没有this.number的值，因此是undefined
+obj.fn1(); // 调用方式， this.number等于obj的number = 3， num = 3; number = 3 * 9 = 27; 此时obj.number = 6
+console.log(window.number, 'window number = 20');
+console.log(obj.number, 'object_number = 6');
