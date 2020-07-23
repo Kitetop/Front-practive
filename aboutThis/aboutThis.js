55e749e74122fa756e22fb0703146c47b99ec973
Kitetop <33886685+Kitetop@users.noreply.github.com>
2020-07-23 11:29:45 +0800

    (add): 关于this指向的补充

--- aboutThis/aboutThis.js
+++ aboutThis/aboutThis.js
@@ -22,3 +22,11 @@ let test = new fn1(); // this是函数自身的this，并没有this.number的值
 obj.fn1(); // 调用方式， this.number等于obj的number = 3， num = 3; number = 3 * 9 = 27; 此时obj.number = 6
 console.log(window.number, 'window number = 20');
 console.log(obj.number, 'object_number = 6');
+/**
+ * 补充：
+ * 不管方法被书写在什么位置，它的this指向只跟着调用方法
+ * | 特殊情况（100%指向全局window）
+ *   | 立即执行函数
+ *   | setTimeout中传入的函数
+ *   | setInterval中传入的函数
+ */
